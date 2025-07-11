57689f540fea2a3804bfeaf39b3579a9a46a2d1f
Giovanni Aguiar De Sousa Bernardes Figueiredo <gaguiar-@c2r3p5.42sp.org.br>
2025-07-11 11:36:27 -0300

    ex15

---
+++ reloaded/ex15/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 11:35:55 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/07/11 11:35:56 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+
+}
+/*
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+int main(void)
+{
+	ft_putstr("teste com espaço");
+}
+*/
