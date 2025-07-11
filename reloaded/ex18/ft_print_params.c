90ee18896a24aea76310e3854383595f33377f9f
Giovanni Aguiar De Sousa Bernardes Figueiredo <gaguiar-@c2r3p5.42sp.org.br>
2025-07-11 13:20:35 -0300

    ex18

---
+++ reloaded/ex18/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 13:20:02 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/07/11 13:20:04 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		while (*argv[i])
+		{
+			ft_putchar(*argv[i]);
+			argv[i]++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+/*
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+*/
