7b18c0a98a332a77b8cd07ef56d435cf80053653
Giovanni Aguiar De Sousa Bernardes Figueiredo <gaguiar-@c2r3p5.42sp.org.br>
2025-07-11 10:56:04 -0300

    ex12 fixed, logic improved

--- reloaded/ex12/ft_iterative_factorial.c
+++ reloaded/ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 16:23:42 by gaguiar-          #+#    #+#             */
-/*   Updated: 2025/07/11 10:44:21 by gaguiar-         ###   ########.fr       */
+/*   Updated: 2025/07/11 10:54:59 by gaguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ int	ft_iterative_factorial(int nb)
 {
 	int	aux;
 
+	aux = 1;
 	if (nb < 0)
 	{
-		aux = 0;
+		return (0);
 	}
 	else if (nb == 0)
 	{
-		aux = 1;
+		return (1);
 	}
 	else
 	{
-		aux = nb;
-		while (nb > 1)
+		while (nb >= 1)
 		{
-			aux = aux * (nb - 1);
+			aux = nb * aux;
 			nb--;
 		}
 		return (aux);
