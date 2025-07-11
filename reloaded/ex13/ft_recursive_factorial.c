e4eede3097f9df70cacfbd95e183b47734786ce1
Giovanni Aguiar De Sousa Bernardes Figueiredo <gaguiar-@c2r3p5.42sp.org.br>
2025-07-11 11:12:10 -0300

    ex13

---
+++ reloaded/ex13/ft_recursive_factorial.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gaguiar- <gaguiar-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 11:11:10 by gaguiar-          #+#    #+#             */
+/*   Updated: 2025/07/11 11:11:18 by gaguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb);
+
+int	ft_recursive_factorial(int nb)
+{
+	int	aux;
+
+	if (nb < 0)
+	{
+		return (0);
+	}
+	else if (nb == 0 || nb == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		aux = nb * ft_recursive_factorial(nb - 1);
+		return (aux);
+	}
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	int result;
+	result = ft_recursive_factorial(6);
+	printf("Resposta: %i\n", result);
+}
+*/
